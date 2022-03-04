97d5148217a37daba0fa6e6b674268f8dbeec46a
VikrantKS <96419531+VikrantKS@users.noreply.github.com>
2022-03-04 16:01:16 +0530

    added connection abort functionality if duplicate connection requested

--- pkg/synapse/synapse.go
+++ pkg/synapse/synapse.go
@@ -19,21 +19,25 @@ import (
 
 // All constant related to synapse
 const (
-	Repo    = "repo"
-	BuildID = "build-id"
-	JobID   = "job-id"
-	Mode    = "mode"
-	ID      = "id"
+	Repo                   = "repo"
+	BuildID                = "build-id"
+	JobID                  = "job-id"
+	Mode                   = "mode"
+	ID                     = "id"
+	DuplicateConnectionErr = "Duplicate connection"
+	AuthenticationFailed   = "Authentication failed"
 )
 
 type synapse struct {
-	conn              *websocket.Conn
-	runner            core.DockerRunner
-	secretsManager    core.SecretsManager
-	logger            lumber.Logger
-	MsgErrChan        chan struct{}
-	MsgChan           chan []byte
-	ConnectionAborted chan struct{}
+	conn                     *websocket.Conn
+	runner                   core.DockerRunner
+	secretsManager           core.SecretsManager
+	logger                   lumber.Logger
+	MsgErrChan               chan struct{}
+	MsgChan                  chan []byte
+	ConnectionAborted        chan struct{}
+	InvalidConnectionRequest chan struct{}
+	LogoutRequired           bool
 }
 
 // New returns new instance of synapse
@@ -44,27 +48,32 @@ func New(
 ) core.SynapseManager {
 
 	return &synapse{
-		runner:         runner,
-		logger:         logger,
-		secretsManager: secretsManager,
-		MsgChan:        make(chan []byte, 1024),
+		runner:                   runner,
+		logger:                   logger,
+		secretsManager:           secretsManager,
+		MsgErrChan:               make(chan struct{}),
+		InvalidConnectionRequest: make(chan struct{}),
+		MsgChan:                  make(chan []byte, 1024),
+		ConnectionAborted:        make(chan struct{}, 10),
+		LogoutRequired:           true,
 	}
 }
 
 func (s *synapse) InitiateConnection(
 	ctx context.Context,
 	wg *sync.WaitGroup,
-) {
+	connectionFailed chan struct{}) {
 	defer wg.Done()
-	go s.openAndMaintainConnection(ctx)
-	s.login()
+	go s.openAndMaintainConnection(ctx, connectionFailed)
 	<-ctx.Done()
-	s.logout()
+	if !s.LogoutRequired {
+		s.logout()
+	}
 	s.runner.KillRunningDocker(context.TODO())
 	s.logger.Debugf("exiting synapse")
 }
 
-func (s *synapse) openAndMaintainConnection(ctx context.Context) {
+func (s *synapse) openAndMaintainConnection(ctx context.Context, connectionFailed chan struct{}) {
 	// setup exponential backoff for retrying control websocket connection
 	var policy = backoff.NewExponential(
 		backoff.WithInterval(500*time.Millisecond),           // base interval
@@ -79,7 +88,7 @@ func (s *synapse) openAndMaintainConnection(ctx context.Context) {
 	s.logger.Debugf("starting socket connection")
 	s.logger.Errorf("starting socket connection at URL %s", global.SocketURL[viper.GetString("env")])
 	for backoff.Continue(b) {
-
+		s.logger.Debugf("trying to connect to lamdatest server")
 		select {
 		case <-ctx.Done():
 			return
@@ -87,35 +96,41 @@ func (s *synapse) openAndMaintainConnection(ctx context.Context) {
 			conn, _, err := websocket.DefaultDialer.Dial(global.SocketURL[viper.GetString("env")], nil)
 			if err != nil {
 				s.logger.Errorf("error connecting synapse to lambdatest %+v", err)
-				s.logger.Warnf("Retrying ...")
-				s.ConnectionAborted <- struct{}{}
 				continue
-
 			}
 			s.conn = conn
 			s.logger.Debugf("synapse connected to lambdatest server")
+			s.login()
 			go s.handleIncomingMessage(normalCloser)
 			go s.WriteMessage()
 			select {
 			case <-ctx.Done():
-				conn.Close()
 				s.ConnectionAborted <- struct{}{}
 				return
 			case <-normalCloser:
 				conn.Close()
 				s.ConnectionAborted <- struct{}{}
 				return
+			case <-s.InvalidConnectionRequest:
+				conn.Close()
+				s.ConnectionAborted <- struct{}{}
+				connectionFailed <- struct{}{}
+				s.LogoutRequired = false
+				return
 			case <-s.MsgErrChan:
 				s.logger.Errorf("Connection between synpase and lambdatest break")
-				s.logger.Warnf("Retrying ...")
 				s.ConnectionAborted <- struct{}{}
 				conn.Close()
-
 			}
+			s.MsgErrChan = make(chan struct{})
+			go s.openAndMaintainConnection(ctx, connectionFailed)
+			return
 
 		}
 	}
-
+	s.logger.Errorf("Unable to establish connection with lambdatest server. exiting...")
+	connectionFailed <- struct{}{}
+	s.LogoutRequired = false
 }
 
 func (s *synapse) handleIncomingMessage(normalCloser chan struct{}) {
@@ -126,6 +141,7 @@ func (s *synapse) handleIncomingMessage(normalCloser chan struct{}) {
 		if err := s.conn.WriteMessage(websocket.PongMessage, nil); err != nil {
 			s.logger.Errorf("Error in writing pong msg %s", err.Error())
 			s.MsgErrChan <- struct{}{}
+			close(s.MsgErrChan)
 			return err
 		}
 		return nil
@@ -141,6 +157,7 @@ func (s *synapse) handleIncomingMessage(normalCloser chan struct{}) {
 			}
 			s.logger.Errorf("disconnecting from lambdatest server. error in reading message %v", err)
 			s.MsgErrChan <- struct{}{}
+			close(s.MsgErrChan)
 			return
 		}
 		s.processMessage(msg)
@@ -157,6 +174,7 @@ func (s *synapse) processMessage(msg []byte) {
 	switch message.Type {
 	case core.MsgError:
 		s.logger.Debugf("error message received from server")
+		go s.processErrorMessage(message)
 	case core.MsgInfo:
 		s.logger.Debugf("info message received from server")
 	case core.MsgTask:
@@ -165,6 +183,15 @@ func (s *synapse) processMessage(msg []byte) {
 	default:
 		s.logger.Errorf("message type not found")
 	}
+}
+
+func (s *synapse) processErrorMessage(message core.Message) {
+	errMsg := string(message.Content)
+	s.logger.Errorf("error message received from server, error %s ", errMsg)
+	if errMsg == DuplicateConnectionErr || errMsg == AuthenticationFailed {
+		s.InvalidConnectionRequest <- struct{}{}
+	}
+
 }
 
 func (s *synapse) processTask(message core.Message) {
@@ -240,8 +267,18 @@ func (s *synapse) login() {
 }
 
 func (s *synapse) logout() {
+	s.logger.Infof("Logging out from lambdatest server")
 	logoutMessage := CreateLogoutMessage()
-	s.SendMessage(&logoutMessage)
+	messageJson, err := json.Marshal(logoutMessage)
+
+	if err != nil {
+		s.logger.Errorf("error marshaling message")
+		return
+	}
+	if err := s.conn.WriteMessage(websocket.TextMessage, messageJson); err != nil {
+		s.logger.Errorf("error sending message to the server, error %v", err)
+
+	}
 }
 
 func (s *synapse) sendResourceUpdates(
@@ -274,9 +311,10 @@ func (s *synapse) WriteMessage() {
 			return
 		case messageJson := <-s.MsgChan:
 			if err := s.conn.WriteMessage(websocket.TextMessage, messageJson); err != nil {
-				s.logger.Errorf("error sending message to the server")
+				s.logger.Errorf("error sending message to the server error %v", err)
 				s.MsgChan <- messageJson
 				s.MsgErrChan <- struct{}{}
+				close(s.MsgErrChan)
 				return
 			}
 		}
